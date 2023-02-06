d434759ca20fbeed7ae15a77c5f3c70f8e394ce1
windofwind <windofwind@gmail.com>
2023-02-07 01:29:06 +0900

    2

--- api/index.ts
+++ api/index.ts
@@ -24,7 +24,7 @@ app.get('/api/item/:slug', (req, res) => {
 });
 
 // Initialize server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Running on port 5000.');
 });
 

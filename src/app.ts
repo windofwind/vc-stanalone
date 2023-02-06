47700eb84eeca91490f450a1cf8e96e5c72d9e20
windofwind <windofwind@gmail.com>
2023-02-07 00:30:48 +0900

    vercel test

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+// Add Express
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get('/', (req, res) => {
+  res.send('Express on Vercel');
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log('Running on port 5000.');
+});
